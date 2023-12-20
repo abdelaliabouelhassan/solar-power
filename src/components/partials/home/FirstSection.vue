23d558a63a6cb01d93c39801b582f594e511c5ba
abdelaliabouelhassan <abdelaliabouelhassan@gmail.com>
2023-12-20 21:33:50 +0100

    changes

--- src/components/partials/home/FirstSection.vue
+++ src/components/partials/home/FirstSection.vue
@@ -1,5 +1,5 @@
 <template>
-    <section class=" w-full bg-[#F4F0BB] py-24 space-y-20 px-4  ">
+    <section class=" w-full bg-[#F4F0BB] py-24 space-y-20 px-4 ml-4  ">
        <div class=" w-full max-w-[1141px] mx-auto flex items-center justify-between relative z-40">
         <div class=" w-full max-w-[28.4375rem] h-[18.6875rem] bg-gray-300 overflow-hidden rounded-lg">
             <img src="/images/home/solarcell.png" class=" w-full h-full object-cover" alt="">
@@ -15,10 +15,10 @@
             <div class=" w-full max-w-[28.4375rem] h-[18.6875rem] bg-gray-300 overflow-hidden rounded-lg">
                 <img src="/images/home/solarcell.png" class=" w-full h-full object-cover" alt="">
             </div>
-            <div class=" -ml-6">
+            <div >
                 <PowerCable />
             </div>
-            <div class="max-w-[31.125rem] h-[20.4375rem] -space-y-[21.3rem] -ml-10">
+            <div class="max-w-[31.125rem] h-[20.4375rem] -space-y-[21.3rem] ">
                 <div class=" w-full max-w-[29.125rem] h-[20.4375rem] bg-[#87C38F] overflow-hidden rounded-lg "></div>
                 <div class=" w-full max-w-[29.125rem] h-[20.4375rem] bg-[#226F54] overflow-hidden rounded-lg ml-3 "></div>
                 <div class=" w-full max-w-[29.125rem] h-[20.4375rem] bg-gray-300 overflow-hidden rounded-lg ml-6 border-[3px] border-[#43291F] ">
@@ -27,7 +27,18 @@
             
             </div>
        </div>
-        <div class=" w-full max-w-[1141px] mx-auto flex items-center justify-between relative z-40">
+       <div class=" w-full max-w-[1141px] mx-auto flex items-center justify-between relative z-40">
+        <div class=" w-full max-w-[28.4375rem] h-[18.6875rem] bg-gray-300 overflow-hidden rounded-lg">
+            <img src="/images/home/solarcell.png" class=" w-full h-full object-cover" alt="">
+        </div>
+            <div>
+                <PowerCable />
+            </div>
+        <div style="box-shadow: 0px 4px 29.8px 6px rgba(0, 0, 0, 0.50);" class=" w-full max-w-[31.125rem] h-[20.4375rem] bg-gray-300 overflow-hidden rounded-lg border-[3px] border-[#87C38F]">
+             <img src="/images/home/solarcell.png" class=" w-full h-full object-cover" alt="">
+        </div>
+       </div>
+        <!-- <div class=" w-full max-w-[1141px] mx-auto flex items-center justify-between relative z-40">
         <div class=" relative w-full max-w-[28.4375rem] h-[18.6875rem] bg-gray-300 overflow-hidden rounded-lg">
             <img src="/images/home/solarcell.png" class=" w-full h-full object-cover" alt="">
 
@@ -62,7 +73,7 @@
                 </div>
             </div>
         </div>
-       </div>
+       </div> -->
 
 
        <div class=" w-full max-w-[46.25rem] mx-auto bg-[#87C38F] h-[20.97325rem] rounded-xl border-4 border-[#43291F] overflow-hidden z-40 relative">
